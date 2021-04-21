7621d454d5493e84fb02f73726a09ac32a9cd025
Alec Chen <alecchendev@gmail.com>
2021-04-20 19:53:06 -0500

    Interface IOHandler class

--- include/io_handler.hpp
+++ include/io_handler.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <utility> // std::pair
+
+using std::string;
+using std::vector;
+using std::pair;
+
 namespace wikigraphs { 
 
 class IOHandler {
-  public: 
-  private: 
+ public: 
+
+  vector<string> ReadArticles(const string & file_name);
+
+  vector<pair<string, string>> ReadLinks(const string & file_name);
+
+ private: 
 };
 
 }
--- src/io_handler.cpp
+++ src/io_handler.cpp
@@ -3,5 +3,16 @@
 namespace wikigraphs {
 
 
+vector<string> IOHandler::ReadArticles(const string & file_name) {
+
+    return vector<string>();
+
+}
+
+vector<pair<string, string>> IOHandler::ReadLinks(const string & file_name) {
+
+    return vector<pair<string, string>>();
+
+}
 
 }
