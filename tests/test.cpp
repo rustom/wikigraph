98ae851ebb43c0e12ad9c96279bd6fe64293cf95
Rustom Ichhaporia <rustomi2@illinois.edu>
2021-05-02 19:20:07 -0500

    Add shortest path test

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,6 +14,7 @@
 
 using wikigraphs::Articles;
 using std::string;
+using std::vector;
 using std::unordered_map;
 using std::unordered_set;
 
@@ -21,14 +22,25 @@ TEST_CASE("Loading in data files") {
   REQUIRE(1 == 1);
   Articles articles("data/tests/articles.tsv", "data/tests/links.tsv");
   unordered_map<string, unordered_set<string>> structure = articles.GetArticles();
-  articles.PrintArticleMap();
 
   unordered_map<string, unordered_set<string>> solution;
   solution.insert({"1", unordered_set<string>({"1", "2"})});
   solution.insert({"2", unordered_set<string>({"3"})});
   solution.insert({"3", unordered_set<string>({"4"})});
-  solution.insert({"4", unordered_set<string>({})});
-  std::cout << "hellO";
+  solution.insert({"4", unordered_set<string>({"1"})});
+
   REQUIRE(solution == structure);
 }
 
+TEST_CASE("Shortest paths") {
+  Articles articles("data/tests/articles.tsv", "data/tests/links.tsv");
+
+  SECTION("Unweighted") {
+    REQUIRE(articles.ShortestPathUnweighted("1", "4") == vector<string>({"1", "2", "3", "4"}));
+  } 
+
+  SECTION("Weighted") {
+    // Fix this output
+    REQUIRE(articles.ShortestPathWeighted("4", "1") == vector<string>({"1", "2", "3" }));
+  }
+}
