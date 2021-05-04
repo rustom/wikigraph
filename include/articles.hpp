8e25b74175efbcd58ca521f53d72773102e6d797
Ayan Mallik <amallik2@illinois.edu>
2021-05-03 21:26:31 -0500

    Document header files to describe articles/io_handler classes

--- include/articles.hpp
+++ include/articles.hpp
@@ -16,22 +16,52 @@ using std::unordered_set;
 
 namespace wikigraphs {
 
+/**
+ * Articles stores the graph of articles 
+ * Nodes represent Wikipedia articles
+ * Edges represent links from one article to another
+ */
 class Articles {
  public: 
+  /**
+   * Builds the article graph by loading in the articles/links from the respective files
+   * Takes in strings that act as file paths to dataset
+   */
   Articles(const string & article_path, const string & link_path);
 
+  /**
+   * Adds a new article with no links to the unordered map
+   */
   void AddArticle(const string & article);
 
+  /**
+   * Adds a list of new articles with no links
+   */
   void AddArticles(const vector<string> & articles);
 
+  /**
+   * Adds a new link to an existing article in the unordered map
+   */
   void AddLink(const string & article, const string & link);
 
+  /**
+   * Adds a list of article-link pairs to the unordered map
+   */
   void AddLinks(const vector<pair<string, string>> & links);
 
+  /**
+   * Prints the content of the article graph
+   */
   void PrintArticleMap();
 
+  /**
+   * Gets a copy of the unordered map that contains the articles/links within the graph
+   */
   unordered_map<string, unordered_set<string>> GetArticles();
 
+  /**
+   * Gets the list of links within the specified article
+   */
   unordered_set<string> GetLinkedArticles(const string & article);
 
   vector<string> ShortestPathUnweighted(const string & source, const string & target);
@@ -61,10 +91,15 @@ class Articles {
   Iterator end();
 
  private:
+  /**
+   * Contains the articles/links within the article graph
+   * Each key maps to a different article
+   * Values store the list of links at the specified article
+   */
   unordered_map<string, unordered_set<string>> articles;
 
   /**
-   * Binary search helper for dijkstra's
+   * Binary search helper for Dijkstra's algorithm
    * Finds index within list of sorted articles according to path weight in unordered_map values - descending order
    */
   size_t FindIndex(const vector<string> & sorted_list, const unordered_map<string, int> & values, int value);
--- include/io_handler.hpp
+++ include/io_handler.hpp
@@ -14,11 +14,21 @@ using std::ifstream;
 
 namespace wikigraphs { 
 
+/**
+ * IOHandler parses the files containing the articles/links
+ * Extracts the files' contents to form the structure of the article graph\
+ */
 class IOHandler {
  public: 
-
+  /**
+   * Reads the list of articles that act as the keys for the article graph
+   */
   vector<string> ReadArticles(const string & file_name);
 
+  /**
+   * Reads the file containing the list of article-list pairs
+   * Returns the values within the article graph
+   */
   vector<pair<string, string>> ReadLinks(const string & file_name);
 
  private: 
