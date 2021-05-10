05d97c0849d533d8d7836286043c99e6d2eb1e9f
Ayan Mallik <amallik2@illinois.edu>
2021-05-10 18:10:09 -0500

    Integrate command link arguments into main function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include "include/io_handler.hpp"
 #include "include/articles.hpp"
 
-int main() {
-  wikigraphs::Articles articles("data/articles.tsv", "data/links.tsv");
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    throw std::invalid_argument("Must include 2 arguments: paths to articles list and mapping of links");
+  }
 
-  // articles.PrintArticleMap();
+  string articles_path = argv[1];
+  string links_path = argv[2];
+
+  wikigraphs::Articles articles(articles_path, links_path);
+
+  //articles.PrintArticleMap();
 
   // for (auto it = articles.begin(); it != articles.end(); ++it) {
   //   std::cout << *it << std::endl;
